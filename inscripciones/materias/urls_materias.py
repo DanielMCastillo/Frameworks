f657cd4e3bd5992f287221538db8a0c3d35a4b78
Daniel Castillo <danielalejandromoralescastillo@gmail.com>
2023-03-04 22:59:40 -0600

    Templates y diseño bootstrap
    Materias, Unidades y Programas

--- inscripciones/materias/urls_materias.py
+++ inscripciones/materias/urls_materias.py
@@ -2,7 +2,7 @@ from django.urls import path
 from materias import views
 
 urlpatterns = [
-    path('', views.ListaMaterias.as_view(), name='lista_programas'),
+    path('', views.ListaMaterias.as_view(), name='lista_materias'),
     
     
 ]
